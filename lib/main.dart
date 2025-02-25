b302da0fc80eb1e18b7359a51bb9a033fcbdf531
aldosaucedo <a.22308051281310@cbtis128.edu.mx>
2025-02-25 17:07:29 +0000

    imagenes aldo

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,61 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MiImagen());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MiImagen extends StatelessWidget {
+  const MiImagen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Mis Imágenes"),
+          backgroundColor: Colors.green,
         ),
-      ),
-    );
-  }
-}
+        body: Center(
+          child: Column(
+            // Usamos Column para mostrar las imágenes una arriba de la otra
+            mainAxisAlignment: MainAxisAlignment
+                .center, // Centramos las imágenes verticalmente
+            children: <Widget>[
+              const Text(
+                  "aldo daniel saucedo de luna 22308051281310"), // Texto adicional
+              Image.network(
+                'https://raw.githubusercontent.com/aldosaucedo1213/imagenes-para-APP-flutter-6J/refs/heads/main/tamal2.jpg', // URL de la primera imagen
+                width: 200, // Ancho opcional
+                height: 200, // Alto opcional
+              ),
+              const SizedBox(height: 20), // Espacio entre las imágenes
+              const Text(
+                "Tamal oaxaqueño en GitHub",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontStyle: FontStyle.italic,
+                  color: Colors.black87,
+                ),
+              ), // Texto descriptivo
+              const SizedBox(
+                  height: 20), // Espacio entre el texto y la siguiente imagen
+              const Text(
+                "Tamal rojo en GitHub",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontStyle: FontStyle.italic,
+                  color: Color(0xdd2e1111),
+                ),
+              ), // Segundo texto descriptivo
+              const SizedBox(height: 20), // Espacio adicional
+              Image.network(
+                'https://raw.githubusercontent.com/aldosaucedo1213/imagenes-para-APP-flutter-6J/refs/heads/main/tamales1.jpg', // URL de la segunda imagen
+                width: 200, // Ancho opcional
+                height: 200, // Alto opcional
+              ),
+            ],
+          ),
+        ),
+      ), // Scaffold
+    ); // MaterialApp
+  } // Widget build
+} // Class MiImagen
